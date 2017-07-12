fb9c9a657451bb6a4947be62376c02cdf0b5fd61
TrCaM <cmthscb@gmail.com>
2017-07-11 22:43:18 -0400

    clone in specify dir

--- playGit.py
+++ playGit.py
@@ -14,23 +14,24 @@ def main():
 
     This function run as a starting point of the program
     """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='playGit.py')
     parser.add_argument("-u", "--user", help ="authorize with github username")
     parser.add_argument("-t", "--token", help = "authorize with github token")
     parser.add_argument("-s", "--save", help = "save the provided information (token or username)",
                          action="store_true")
-    parser.add_argument("-c", "--clone", help= "clone all repository in SCS-Carleton beginning with the pattern")
+    parser.add_argument("-c", "--clone", metavar= 'prefix',  help= "clone all repository in SCS-Carleton beginning with the pattern")
+    parser.add_argument("-d", "--dir", metavar= 'directory',  help= "the directory path to save the clone folder")
     args = parser.parse_args()
     try:
         with open("./data/data.json", "r") as f:
             data = json.load(f)
     except IOError as e:
-        data = {'user': None, 'token' : None}
+        data = {'user': None, 'token' : None, 'dir': None}
 
-    if args.save and not (args.user or args. token):
+    if args.save and not (args.user or args. token or args.dir):
         print("TAtool: no information to save")
         print("TAtool: try 'TAtool --help' for more information")
-    elif args.user or args.token:
+    elif args.user or args.token or args.dir:
         if args.user:
             data['user'] = args.user
         if args.token:
@@ -51,11 +52,21 @@ def main():
             git = connect_to_git(data['token'])
         SCS = git.get_organization("SCS-Carleton")
         count =0
+
+        if args.dir:
+            if os.path.isdir(args.dir):
+                data['dir'] = os.path.abspath(args.dir)
+            else:
+                raise Exception
+        else:
+            data['dir'] = './'
+        with safe_open_w("./data/data.json") as f:
+            json.dump(data, f, ensure_ascii = False)
         # Set up credential helper
         for repo in SCS.get_repos():
             if repo.name.startswith(args.clone):
                 count+=1
-                clone_repo(repo.name)
+                clone_repo(repo.name, data['dir'])
         print("There are total " + str(count) + " submissions cloned")
 
 def connect_to_git(token=None, login=None):
@@ -74,23 +85,23 @@ def connect_to_git(token=None, login=None):
         password = getpass.getpass()
         return Github(login, password)
 
-def clone_repo(repo_name, *org_or_user):
+def clone_repo(repo_name, dir_path, *org_or_user):
     """Clone a remote repository
 
     Initiate a subprocess that call git to be launched and clone the specified repo_name
 
     Args:
         repo_name: the name of the repository
-
+        dir_path : the directories that user wants to clone into
         org_or_user: the name of the organization or user that the repo belongs to (Default: "SCS-Caleton")
 
     """
     try:
         print("Cloning ", repo_name, "...")
         if not org_or_user:
-            subprocess.run(["git", "clone", "https://github.com/SCS-Carleton/" + repo_name + ".git", "./submissions/" + repo_name])
+            subprocess.run(["git", "clone", "https://github.com/SCS-Carleton/" + repo_name + ".git", dir_path +"submissions/" + repo_name])
         else:
-            subprocess.run(["git", "clone", "https://github.com/"+ "/".join(org_or_user) + "/" + repo_name + ".git", "./submissions/" + repo_name])
+            subprocess.run(["git", "clone", "https://github.com/"+ "/".join(org_or_user) + "/" + repo_name + ".git", dir_path + "submissions/" + repo_name])
 
     except Exception as e:
         print(e.message)
