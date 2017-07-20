40f121915ac1a179164096d2adc5e66af7260cf0
TrCaM <TriCao@cmail.carleton.ca>
2017-07-20 11:03:45 -0400

    some of export

--- export.py
+++ export.py
@@ -1,2 +1,30 @@
+#!/usr/bin/python3
 ''' This script to export feedback file from submissions
 '''
+
+import sys
+import os
+import argparse
+def main():
+
+    args = args_handle()
+
+    if os.path.isdir(args.input[1]) or os.path.isdir("./" +args.input[1]):
+        print("Will get ", args.input[0],"  from here")
+    else:
+        print("The folder doesn't exist")
+
+
+
+def args_handle():
+    ''' Handle input options via command line arguments
+    '''
+    parser = argparse.ArgumentParser(prog='export.py')
+    parser.add_argument("input", nargs=2, help="input path for the directory",
+                        default= "./")
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    main()
