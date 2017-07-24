eba90ab0c943f5b6dc517f5ea791c6c6c614a085
trungdinh <dinhthanhtrung1996@gmail.com>
2017-07-24 07:05:22 +0200

    restructure for packaging

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ from setuptools import setup
 setup(name='gitclassroom',
       version='0.1',
       description="Package supports auto-marking student's assignment on GitHub",
-      classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.x',
-      ],
       url='https://github.com/TrCaM/1406-github',
       author='Tri Cao',
       author_email='cmthscb@gmail.com',
